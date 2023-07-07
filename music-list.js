bb7ef21845a7630941d733cc1f42470cdbcc6c36
ErenNroballytb <jx2phatem011@gmail.com>
2023-07-07 14:42:10 +0700

    erenx

---
+++ music-list.js
@@ -0,0 +1,41 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Em Ơi Anh Phải Làm Sao Remix 8D",
+    artist: "Dương Minh Tuấn,Eren Mix 8D",
+    img: "Em Ơi Anh Phải Làm Sao Remix 8D",
+    src: "Em Ơi Anh Phải Làm Sao Remix 8D"
+  },
+  {
+    name: "Quá Khứ Anh Không Thể Quên 8D",
+    artist: "Dương Minh Tuấn,Eren Mix 8D",
+    img: "Quá Khứ Anh Không Thể Quên 8D",
+    src: "Quá Khứ Anh Không Thể Quên 8D"
+  },
+  {
+    name: "Hoa Cỏ Lau Mix 8D",
+    artist: "Phong Max,Eren Mix 8D",
+    img: "Hoa Cỏ Lau Mix 8D",
+    src: "Hoa Cỏ Lau Mix 8D"
+  },
+  {
+    name: "Cứ Thế Mong Chờ Mix 8D",
+    artist: "Nguyễn Đình Vũ,Eren Mix 8D",
+    img: "Cứ Thế Mong Chờ Mix 8D",
+    src: "Cứ Thế Mong Chờ Mix 8D"
+  },
+  {
+    name: "Hồi Ức Ta Đã Yêu Mix 8D",
+    artist: "Linh Hương Luz,Eren Mix 8D",
+    img: "Hồi Ức Ta Đã Yêu Mix 8D",
+    src: "Hồi Ức Ta Đã Yêu Mix 8D"
+  },
+];
